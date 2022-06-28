e78ae0ff80e4f4a6433fc4cd4b1b2a257b94403a
Alex Chemus <aachemus@gmail.com>
2022-06-28 10:15:30 +0500

    back to old redirect uri

--- src/redux/initState.ts
+++ src/redux/initState.ts
@@ -3,7 +3,7 @@ import { State } from "../utils/types"
 const initState: State = {
   clientId: '_3z7Jj5krcWsmoEexP0f3Q',
   //redirectUri: 'https://cloned-reddit.herokuapp.com/redirected',
-  redirectUri: 'https://alex-chemus.github.io/yoomer/#/redirected',
+  redirectUri: 'https://alex-chemus.github.io/yoomer/redirected',
   accessToken: null,
   baseUrl: 'https://oauth.reddit.com',
   isFetchingToken: false,
