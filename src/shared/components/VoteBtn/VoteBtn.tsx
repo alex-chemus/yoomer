e63c1b493adbf21f4bec1fcb561591be3f33cb2e
Alex Chemus <aachemus@gmail.com>
2022-07-02 13:47:47 +0500

    update VoteBtn

--- src/shared/components/VoteBtn/VoteBtn.tsx
+++ src/shared/components/VoteBtn/VoteBtn.tsx
@@ -9,7 +9,7 @@ interface VoteBtnProps {
 }
 
 const VoteBtn: FC<VoteBtnProps> = ({ likes, check, onClick, bgcolor }) => {
-  const cls = classes.btn
+  const cls = [classes.btn]
   if (check && likes === 1) cls.push(classes.upvote)
   if (check && likes === -1) cls.push(classes.downvote)
   if (likes === -1) cls.push(classes.rotated)
@@ -17,7 +17,7 @@ const VoteBtn: FC<VoteBtnProps> = ({ likes, check, onClick, bgcolor }) => {
   return (
     <button 
         onClick={() => onClick(likes)}
-        className={cls}
+        className={cls.join(' ')}
         style={{backgroundColor: bgcolor}}
       >
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
