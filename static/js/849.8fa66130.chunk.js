8aa5295477d7eedd387f6b56c7b9e29580f7bacf
Alex Chemus <aachemus@gmail.com>
2022-07-05 10:33:51 +0500

    Updates

--- static/js/849.8fa66130.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkyoomer=self.webpackChunkyoomer||[]).push([[849],{1849:function(e,u,o){o.r(u);var r=o(2711);u.default=r.Ij}}]);
-//# sourceMappingURL=849.8fa66130.chunk.js.map
