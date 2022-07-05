f53fc8275e7d544955925966e53f4d92682c80fb
Alex Chemus <aachemus@gmail.com>
2022-07-05 19:15:59 +0500

    Updates

--- static/js/849.22591d10.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkyoomer=self.webpackChunkyoomer||[]).push([[849],{1849:function(e,u,o){o.r(u);var r=o(5726);u.default=r.Ij}}]);
-//# sourceMappingURL=849.22591d10.chunk.js.map
---
+++ static/js/849.72d0d9c4.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkyoomer=self.webpackChunkyoomer||[]).push([[849],{1849:function(e,u,o){o.r(u);var r=o(2675);u.default=r.Ij}}]);
+//# sourceMappingURL=849.72d0d9c4.chunk.js.map
